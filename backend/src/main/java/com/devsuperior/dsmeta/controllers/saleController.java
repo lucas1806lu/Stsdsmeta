c6597a0548b3f99357dccf659359c9513403e60d
lucas1806lu <lchaves.20@gmail.com>
2022-11-18 12:06:34 -0300

    “Date_Select”

---
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/saleController.java
@@ -0,0 +1,34 @@
+package com.devsuperior.dsmeta.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dsmeta.entities.sale;
+import com.devsuperior.dsmeta.services.saleservice;
+
+
+@RestController
+@RequestMapping(value = "/sales")
+
+public class saleController {
+	
+	
+	@Autowired
+	private saleservice service;
+	
+	@GetMapping
+	public Page <sale>findSales(
+			
+			@RequestParam(value = "minDate", defaultValue ="") String minDate, 
+			@RequestParam(value = "maxDate", defaultValue ="")String maxDate, 
+			Pageable Pageable) {
+		
+		   return service.findSales(minDate, maxDate, Pageable);
+		   
+	}
+}
--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -1,11 +1,18 @@
 package com.devsuperior.dsmeta.repositories;
 
+import java.time.LocalDate;
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.devsuperior.dsmeta.entities.sale;
 
 public interface SaleRepository extends JpaRepository <sale, Long> {
 	
-	
+	@Query("SELECT obj FROM sale obj WHERE obj.date BETWEEN :min AND :max ORDER BY obj.amount DESC")
+	Page<sale> findSales(LocalDate min, LocalDate max, Pageable pageable);
 
 }
